131ad206b1ef97ed539e9b1e416fda7d63b680d9
ShashwatM3 <smahalanobis@ucdavis.edu>
2025-10-02 16:18:14 -0700

    changed landing

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import LandingPage from "./LandingPage"
 
 export default function Home() {
   return (
-    // <LandingPage/>
-    <div className="h-screen w-screen flex items-center justify-center bg-white text-black">
-      <h1 className="text-9xl">ilabucd.com</h1>
-    </div>
+    <LandingPage/>
+    // <div className="h-screen w-screen flex items-center justify-center bg-white text-black">
+    //   <h1 className="text-9xl">ilabucd.com</h1>
+    // </div>
   );
 }
